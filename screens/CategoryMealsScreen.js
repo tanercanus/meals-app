b284709c4a20ffa0496d7bf94862329d9d786b42
tanercanus <tanercanus@yahoo.com>
2020-08-26 00:18:57 +0200

    - Added navigations

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
 const CategoryMealsScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The Category Meals Screen!</Text>
+            <Button title="Go to Deatils!" onPress={ () => {
+                props.navigation.navigate({routeName: 'MealDetail' });
+            } } />
+            <Button title="Go Back" onPress={ () => {
+                //Both of them are the same
+                //props.navigation.goBack();
+                props.navigation.pop();
+            }}/>
         </View>
     );
 };
